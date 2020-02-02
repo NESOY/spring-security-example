b1293ee7da7e6ebe4513d4031466c1f9ee70e475
Kwon Young Jae <kyoje11@gmail.com>
2020-02-02 21:38:34 +0900

    update: in-memory users

--- src/main/java/com/nesoy/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/nesoy/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.nesoy.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +18,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                .anyRequest().authenticated();
        http.formLogin();
        http.httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /**
+         * {암호화 Prefix}
+         */
+        auth.inMemoryAuthentication()
+                .withUser("nesoy").password("{noop}123").roles("USER").and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }
